21002bc9e3f34219800cc708c619a03e29b8b773
meg <megan.daneika@gmail.com>
2020-05-29 12:55:48 +0100

    Add functionality for clearing all items

--- app.js
+++ app.js
@@ -81,6 +81,10 @@ const ItemCtrl = (function () {
       data.items.splice(index, 1);
     },
 
+    clearAllItems: function () {
+      data.items = [];
+    },
+
     setCurrentItem: function (item) {
       data.currentItem = item;
     },
@@ -117,6 +121,7 @@ const UICtrl = (function () {
     updateBtn: ".update-btn",
     deleteBtn: ".delete-btn",
     backBtn: ".back-btn",
+    clearBtn: ".clear-btn",
     itemNameInput: "#item-name",
     itemCaloriesInput: "#item-calories",
     totalCalories: ".total-calories",
@@ -207,6 +212,17 @@ const UICtrl = (function () {
       UICtrl.showEditState();
     },
 
+    removeItems: function () {
+      let listItems = document.querySelectorAll(UISelectors.listItems);
+
+      // Turn Node list into array
+      listItems = Array.from(listItems);
+
+      listItems.forEach(function (item) {
+        item.remove();
+      });
+    },
+
     hideList: function () {
       document.querySelector(UISelectors.itemList).style.display = "none";
     },
@@ -277,6 +293,11 @@ const App = (function (ItemCtrl, UICtrl) {
     document
       .querySelector(UISelectors.backBtn)
       .addEventListener("click", UICtrl.clearEditState);
+
+    // Clear button event
+    document
+      .querySelector(UISelectors.clearBtn)
+      .addEventListener("click", clearAllItemsClick);
   };
 
   // Add item submit
@@ -352,6 +373,7 @@ const App = (function (ItemCtrl, UICtrl) {
     e.preventDefault();
   };
 
+  // Delete button event
   const itemDeletesubmit = function (e) {
     // Get current item
     const currentItem = ItemCtrl.getCurrentItem();
@@ -371,6 +393,24 @@ const App = (function (ItemCtrl, UICtrl) {
     UICtrl.clearEditState();
 
     e.preventDefault();
+  };
+
+  // Clear Items Event
+  const clearAllItemsClick = function () {
+    // Delete all items frrom data structure
+    ItemCtrl.clearAllItems();
+
+    // Get total calories
+    const totalCalories = ItemCtrl.getTotalCalories();
+    // Add total calories to Ui
+    UICtrl.showTotalCalories(totalCalories);
+    //console.log(totalCalories)
+
+    // Remove from UI
+    UICtrl.removeItems();
+
+    // Hide UL
+    UICtrl.hideList();
   };
 
   return {
